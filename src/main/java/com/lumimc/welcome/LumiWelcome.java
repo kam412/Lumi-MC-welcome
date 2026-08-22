383672e42d0a48841a119c485974efce9d0fb10e
kam412 <117849170+kam412@users.noreply.github.com>
2026-08-22 16:26:55 -0400

    Refactor welcome message and firework effects
    Updated welcome message to use Adventure API for better formatting and added color. Adjusted firework spawning logic and effects.

--- src/main/java/com/lumimc/welcome/LumiWelcome.java
+++ src/main/java/com/lumimc/welcome/LumiWelcome.java
@@ -1,5 +1,7 @@
 package com.lumimc.welcome;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
 import org.bukkit.Color;
@@ -21,47 +23,54 @@ public class LumiWelcome extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
 
-        // Send welcome message
-        event.getPlayer().sendMessage(
-                "---------------------------------------"
-        );
+        // Only run for players joining for the FIRST time
+        if (!event.getPlayer().hasPlayedBefore()) {
 
-        event.getPlayer().sendMessage(
-                "Welcome to LumiMC (" +
-                event.getPlayer().getName() +
-                ") we hope you enjoy your stay here!"
-        );
+            String playerName = event.getPlayer().getName();
 
-        event.getPlayer().sendMessage(
-                "---------------------------------------"
-        );
-
-        // Get player's location
-        Location location = event.getPlayer().getLocation();
-
-        // Spawn 3 fireworks
-        for (int i = 0; i < 3; i++) {
-
-            Firework firework = location.getWorld().spawn(
-                    location,
-                    Firework.class
+            // Welcome message
+            event.getPlayer().sendMessage(
+                    Component.text("---------------------------------------")
+                            .color(NamedTextColor.GOLD)
             );
 
-            FireworkMeta meta = firework.getFireworkMeta();
-
-            meta.setPower(1);
-
-            meta.addEffect(
-                    FireworkEffect.builder()
-                            .with(FireworkEffect.Type.BALL_LARGE)
-                            .withColor(Color.AQUA)
-                            .withFade(Color.WHITE)
-                            .flicker(true)
-                            .trail(true)
-                            .build()
+            event.getPlayer().sendMessage(
+                    Component.text("Welcome to LumiMC " + playerName +
+                            " we hope you enjoy your stay here!")
+                            .color(NamedTextColor.GOLD)
             );
 
-            firework.setFireworkMeta(meta);
+            event.getPlayer().sendMessage(
+                    Component.text("---------------------------------------")
+                            .color(NamedTextColor.GOLD)
+            );
+
+            // Fireworks
+            Location location = event.getPlayer().getLocation();
+
+            for (int i = 0; i < 3; i++) {
+
+                Firework firework = location.getWorld().spawn(
+                        location,
+                        Firework.class
+                );
+
+                FireworkMeta meta = firework.getFireworkMeta();
+
+                meta.setPower(1);
+
+                meta.addEffect(
+                        FireworkEffect.builder()
+                                .with(FireworkEffect.Type.BALL_LARGE)
+                                .withColor(Color.ORANGE)
+                                .withFade(Color.YELLOW)
+                                .flicker(true)
+                                .trail(true)
+                                .build()
+                );
+
+                firework.setFireworkMeta(meta);
+            }
         }
     }
 }
