54220214f0f4e0abe1c4f26edf9d8a2b4177739a
kam412 <117849170+kam412@users.noreply.github.com>
2026-08-22 16:23:00 -0400

    Enhance player join event with fireworks
    Added functionality to spawn fireworks when a player joins.

--- src/main/java/com/lumimc/welcome/LumiWelcome.java
+++ src/main/java/com/lumimc/welcome/LumiWelcome.java
@@ -1,10 +1,13 @@
 package com.lumimc.welcome;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
+import org.bukkit.Color;
+import org.bukkit.entity.Firework;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LumiWelcome extends JavaPlugin implements Listener {
@@ -12,26 +15,53 @@ public class LumiWelcome extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("LumiWelcome has been enabled!");
+        getLogger().info("LumiWelcome ENABLED!");
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        String playerName = event.getPlayer().getName();
 
+        // Send welcome message
         event.getPlayer().sendMessage(
-                Component.text("---------------------------------------")
-                        .color(NamedTextColor.GRAY)
+                "---------------------------------------"
         );
 
         event.getPlayer().sendMessage(
-                Component.text("Welcome to LumiMC (" + playerName + ") we hope you enjoy your stay here!")
-                        .color(NamedTextColor.AQUA)
+                "Welcome to LumiMC (" +
+                event.getPlayer().getName() +
+                ") we hope you enjoy your stay here!"
         );
 
         event.getPlayer().sendMessage(
-                Component.text("---------------------------------------")
-                        .color(NamedTextColor.GRAY)
+                "---------------------------------------"
         );
+
+        // Get player's location
+        Location location = event.getPlayer().getLocation();
+
+        // Spawn 3 fireworks
+        for (int i = 0; i < 3; i++) {
+
+            Firework firework = location.getWorld().spawn(
+                    location,
+                    Firework.class
+            );
+
+            FireworkMeta meta = firework.getFireworkMeta();
+
+            meta.setPower(1);
+
+            meta.addEffect(
+                    FireworkEffect.builder()
+                            .with(FireworkEffect.Type.BALL_LARGE)
+                            .withColor(Color.AQUA)
+                            .withFade(Color.WHITE)
+                            .flicker(true)
+                            .trail(true)
+                            .build()
+            );
+
+            firework.setFireworkMeta(meta);
+        }
     }
 }
